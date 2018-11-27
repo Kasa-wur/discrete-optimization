c120299cad374e7a425d76ec62c7079b0bc35a37
Kasa <xinyuan.min2017@gmail.com>
2018-11-27 18:38:40 +0100

    numba + pure_py, slightly faster, cannot solve 300

--- solver.py
+++ solver.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from collections import namedtuple
 import time
-import numpy as np
 from numba import jit
 
-Item = namedtuple("Item", ['index', 'value', 'weight'])
 
 @jit
 def data_parser(input_data):
     lines = input_data.split('\n')
-
     firstLine = lines[0].split()
     item_count = int(firstLine[0])
     capacity = int(firstLine[1])
@@ -19,36 +15,37 @@ def data_parser(input_data):
     for i in range(1, item_count+1):
         line = lines[i]
         parts = line.split()
-        items.append(Item(i-1, int(parts[0]), int(parts[1])))
+        dict = {'index': i-1, 'value' : int(parts[0]),'weight' : int(parts[1])}
+        items.append(dict)
     return capacity, items
 
-
-def ny_matrix(capacity, items):
-    matrix = np.zeros((capacity+1,len(items)))
+def dy_matrix(capacity, items):
+    matrix = [[0 for j in items] for i in range(capacity+1)]
     for w in range(capacity + 1):
-        matrix[w,0] = 0 if w < items[0].weight else items[0].value
-        for item in items[1:]:
-            if item.weight > w:
-                matrix[w,item.index] = matrix[w, item.index-1]
+        matrix[w][0] = 0 if w < items[0]['weight'] else items[0]['value']
+        for i, item in enumerate(items[1:]):
+            ind = i + 1
+            if item['weight'] > w:
+                matrix[w][ind] = matrix[w][ind - 1]
             else:
-                matrix[w, item.index] = max(matrix[w,item.index - 1],\
-                                            matrix[w - item.weight,item.index - 1] + item.value )
+                matrix[w][ind] = max(matrix[w][ind - 1],\
+                                     matrix[w-item['weight']][ind-1]+item['value'])
     return matrix
 
 def trace_back(matrix,items):
-    (row, col) = tuple(map(lambda x: x-1,matrix.shape))
+    row = len(matrix) - 1
+    col = len(matrix[0]) - 1
     taken = {}
     for item in items[::-1]:
-        if row == 0 or col== 0:
-            taken[item.index] = 1 if matrix[row,col] == item.value else 0
-        elif col >= 1 and row >=0 and matrix[row,col] == matrix[row,col-1]:
-            taken[item.index] = 0
+        if col >= 1 and row >=0 and matrix[row][col] == matrix[row][col-1]:
+            taken[item['index']] = 0
+        elif row == 0 or col== 0:
+            taken[item['index']] = 1 if matrix[row][col] == item['value'] else 0
         else:
-            taken[item.index] = 1
-            row -=  item.weight
+            taken[item['index']] = 1
+            row -=  item['weight']
         col -= 1
     return taken
-
 
 #
 # def solve_it(input_data):
@@ -60,24 +57,14 @@ def trace_back(matrix,items):
 #         output_data += str(take[i]) + ' '
 #     return output_data
 
-
-
-
 if __name__ == '__main__':
     t1 = time.clock()
-    with open('data/ks_400_0','r') as input_data_file:
+    with open('data/ks_100_0','r') as input_data_file:
         input_data = input_data_file.read()
         capacity, items = data_parser(input_data)
 
-    # matrix = dy_matrix(capacity, items)
-    # take = trace_back(matrix,items)
-    # print(matrix[-1][-1])
-    # print(take)
-
-    np_mat = ny_matrix(capacity, items)
-    print(np_mat)
-    print('total value:', np_mat[-1,-1])
-    take = trace_back(np_mat, items)
+    matrix = dy_matrix(capacity, items)
+    take = trace_back(matrix,items)
     t2 = time.clock()
     print('run time:%f s'%(t2-t1))
 
